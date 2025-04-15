35773abf4f4a625513b6339fbef11f0a3215ecbd
moonboyotb <moonboyotb@gmail.com>
2025-04-15 09:54:29 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const start = document.querySelector(".start")
+
+setTimeout(() => {
+    start.style.visibility = "visible"
+}, 3500);
+
+// C'est pour afficher le boutton start qui nous emmene au "game.html"
